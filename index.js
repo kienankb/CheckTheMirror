6cf3051200a5704359219bdbc21419906abe50f1
Kienan Knight-Boehm <kienan@kienankb.com>
2015-11-15 04:20:45 -0500

    Cleaned up/modified code and comments

--- index.js
+++ index.js
@@ -7,17 +7,6 @@
 
     or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
 */
-
-/**
- * This simple sample has no external dependencies or session management, and shows the most basic
- * example of how to create a Lambda function for handling Alexa Skill requests.
- *
- * Examples:
- * One-shot model:
- *  User: "Alexa, tell Greeter to say hello"
- *  Alexa: "Hello World!"
- */
-
 /**
  * App ID for the skill
  */
@@ -31,7 +20,7 @@ var Twitter = require('twitter');
 var sentimentAnalysis = require('sentiment-analysis');
 
 /**
- * HelloWorld is a child of AlexaSkill.
+ * GetSentiment is a child of AlexaSkill.
  * To read more about inheritance in JavaScript, see the link below.
  *
  * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Introduction_to_Object-Oriented_JavaScript#Inheritance
@@ -109,7 +98,7 @@ GetSentiment.prototype.intentHandlers = {
 
 // Create the handler that responds to the Alexa Request.
 exports.handler = function (event, context) {
-    // Create an instance of the HelloWorld skill.
+    // Create an instance of the GetSentiment skill.
     var getSentiment = new GetSentiment();
     getSentiment.execute(event, context);
 };
